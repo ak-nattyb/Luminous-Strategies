9c77d771cdb7f67bf476886f15ec7e03dd62443e
Natty Burmeister <natty@akimbo.tv>
2026-05-10 03:05:15 -0700

    added a proper V1 of this

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,5 @@
-import ContentContainer from "@/components/ContentContainer";
 import { FlippableCard } from "@/components/FlippableCard";
-import { MaterialIcons } from "@expo/vector-icons";
-import { View } from "react-native";
 
 export default function Tab() {
-  return (
-    <FlippableCard
-      value={
-        "Short circuit (If eating peas improves virility, shovel them into your pants)"
-      }
-    ></FlippableCard>
-  );
+  return <FlippableCard></FlippableCard>;
 }
